2683e447f91792ea64a314ca27958042644bef9e
DavidMohan <92872489+DavidMohan@users.noreply.github.com>
2022-05-04 20:47:55 +0530

    Events

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -2,5 +2,5 @@ const parallax = document.getElementById('parallax');
 
 window.addEventListener('scroll', function() {
   let offset = this.window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.7 +"px";
+  parallax.style.backgroundPositionY = offset * .9 +"px";
 });
